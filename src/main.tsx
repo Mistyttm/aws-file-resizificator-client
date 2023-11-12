bfe64fda2ba6315ce5ba79fbc58e94be54a9b401
Mistyttm <emmey.leo@gmail.com>
2023-11-12 23:45:51 +1000

    Add routing and update component imports in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import Home from './Home.tsx'
 import './index.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Download from './Filedownload.tsx';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")!).render(
+    <React.StrictMode>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/download" element={<Download />} />
+            </Routes>
+        </BrowserRouter>
+    </React.StrictMode>
+);
